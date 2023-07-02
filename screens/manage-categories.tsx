6417a57f965f33f8fae29bf4551d955f737e4b81
Vijit Ail <vijit2ail@gmail.com>
2023-07-02 11:31:26 +0530

    2 column responsive grid

--- screens/manage-categories.tsx
+++ screens/manage-categories.tsx
@@ -1,8 +1,15 @@
-import { FlatList, KeyboardAvoidingView, StyleSheet, View } from "react-native";
+import {
+  FlatList,
+  KeyboardAvoidingView,
+  StyleSheet,
+  useWindowDimensions,
+  View,
+} from "react-native";
 import ScreenWrapper from "../components/ScreenWrapper";
 import { useCategoryStore } from "../stores/category-store";
 import { AddEditCategory } from "../components/manage-categories/AddEditCategory";
 import { Button, Text } from "react-native-paper";
+import { useEffect, useState } from "react";
 // import KeyboardSpacer from "react-native-keyboard-spacer";
 
 export const ManageCategoriesScreen = () => {
@@ -12,11 +19,26 @@ export const ManageCategoriesScreen = () => {
 
   const addCategory = useCategoryStore((state) => state.addCategory);
 
+  const { width } = useWindowDimensions();
+
+  const columnWidth = 320;
+
+  const calculateColumns = (width: number) => {
+    return width > (columnWidth + 16) * 2 ? 2 : 1;
+  };
+
+  const [columns, setColumns] = useState(1);
+
+  useEffect(() => {
+    setColumns(calculateColumns(width));
+  }, [width]);
+
   return (
     <ScreenWrapper withScrollView={false}>
       <View style={styles.container}>
         <KeyboardAvoidingView behavior="height">
           <FlatList
+            numColumns={columns}
             ListHeaderComponent={() => (
               <View style={styles.addCategoryContainer}>
                 {!categories.length && <Text>No Categories Found!</Text>}
@@ -31,12 +53,28 @@ export const ManageCategoriesScreen = () => {
             )}
             data={categories}
             renderItem={({ item }) => (
-              <View style={{ marginHorizontal: 16 }}>
+              <View
+                style={{
+                  flex: 1,
+                  width: "100%",
+                  maxWidth: columnWidth,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginHorizontal: 8,
+                }}
+              >
                 <AddEditCategory category={item} />
               </View>
             )}
             keyExtractor={(item) => item.id}
+            key={columns}
             ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+            contentContainerStyle={{
+              justifyContent: "center",
+              marginLeft: "auto",
+              marginRight: "auto",
+              width: (columnWidth + 16) * columns,
+            }}
           />
         </KeyboardAvoidingView>
       </View>
